3a60c460639ac83230b51284f9c3acf737a1d15d
Ahamed Aathil <ahamedaathil.5@gmail.com>
2024-11-11 14:49:19 +0530

    frontend: user - homepage UI carousel product cards UI bugfix
    Signed-off-by: Ahamed Aathil <ahamedaathil.5@gmail.com>

--- frontend/src/components/AllProducts.jsx
+++ frontend/src/components/AllProducts.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ItemPreview from './ItemPreview';
+/* eslint-disable react/prop-types */
+import ItemPreview from "./ItemPreview";
 
 const AllProducts = ({ products }) => {
   return (
     <div className="bg-white p-4 shadow-lg mt-4">
       <h2 className="text-xl font-bold mb-3">ALL PRODUCTS</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {products.map(product => (
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+        {products.map((product) => (
           <ItemPreview
             key={product.id}
             name={product.name}
