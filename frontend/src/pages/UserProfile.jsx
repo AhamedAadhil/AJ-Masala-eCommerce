d111231f51800eb26a051980415c824cd43eaace
it21004490 <it21004490@my.sliit.lk>
2024-11-13 12:39:15 +0530

    frontend:UserProfilePage/Logout Btn Added

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import CoverOne from "../assets/cover-01.png";
 import userSix from "../assets/user-06.png";
 import MyOrdersTable from "../components/MyOrdersTable";
+import { LogOut } from "lucide-react";
 
 const UserProfile = () => {
   return (
@@ -12,6 +13,17 @@ const UserProfile = () => {
           alt="profile cover"
           className="h-full w-full rounded-tl-sm rounded-tr-sm object-cover object-center"
         />
+        <div className="absolute top-1 right-1 z-10 xsm:bottom-4 xsm:right-4 bg-red-600 rounded-lg">
+            <label
+              htmlFor="cover"
+              className="flex cursor-pointer items-center justify-center gap-2 rounded bg-primary py-1 px-2 text-sm font-medium text-white hover:bg-opacity-90 xsm:px-4"
+            >
+              <span>
+                <LogOut size={17}/>
+              </span>
+              <span>Logout</span>
+            </label>
+         </div>
       </div>
       <div className="px-4 pb-6 text-center lg:pb-8 xl:pb-11.5">
         <div className="relative z-30 mx-auto -mt-20 lg:h-30 w-full max-w-30 rounded-full bg-white/20 p-1 backdrop-blur sm:h-44 sm:max-w-44 sm:p-3">
