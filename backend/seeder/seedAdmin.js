b2bf3a95a0d3b282a5d6328131bfd4cfe9ed5b50
Ahamed Aathil <ahamedaathil.5@gmail.com>
2024-11-22 17:45:20 +0530

    backend: admin seeder function create
    Signed-off-by: Ahamed Aathil <ahamedaathil.5@gmail.com>

---
+++ backend/seeder/seedAdmin.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import User from "../model/user.model.js";
+import { connectDB } from "../lib/db.js";
+
+dotenv.config();
+
+const seedAdmin = async () => {
+  try {
+    await connectDB();
+    const adminDetails = {
+      name: "Ajmal",
+      email: "ajmal@gmail.com",
+      password: "123456",
+      role: "admin",
+    };
+
+    const adminExist = await User.findOne({ email: adminDetails.email });
+    if (adminExist) {
+      return console.log("Admin already exist");
+    }
+    const newAdmin = new User(adminDetails);
+    await newAdmin.save();
+    console.log("Admin created successfully");
+
+    mongoose.connection.close();
+  } catch (error) {
+    console.error("Error seeding admin:", error.message);
+    process.exit(1);
+  }
+};
+
+// node backend/seeder/seedAdmin.js
+// use this to run the seeder file to create new admin user
+// if need create new admin user run this file again with new user info
+
+seedAdmin();
